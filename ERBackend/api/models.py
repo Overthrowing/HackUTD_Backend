4a71667caf2d852d051b073bd165d268a8f80cc0
Siddharth Radhakrishnan <siddharth.radh@gmail.com>
2023-11-04 22:26:50 -0500

    Formated code

--- ERBackend/api/models.py
+++ ERBackend/api/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Patient(models.Model):
     # Personal Information
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    gender = models.CharField(
+        max_length=10, choices=[("M", "Male"), ("F", "Female"), ("O", "Other")]
+    )
 
     # Contact Information
     # email = models.EmailField(blank=True, null=True)
@@ -17,14 +20,23 @@ class Patient(models.Model):
     # medical_history = models.TextField(blank=True, null=True)
     # medications = models.TextField(blank=True, null=True)
     # allergies = models.TextField(blank=True, null=True)
-    
+
     # Relationships
-    primary_doctor = models.ForeignKey('Doctor', on_delete=models.SET_NULL, blank=True, null=True)
-    room = models.ForeignKey('Room', on_delete=models.SET_NULL, blank=True, null=True, related_name="patients")
+    primary_doctor = models.ForeignKey(
+        "Doctor", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    room = models.ForeignKey(
+        "Room",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="patients",
+    )
     # appointments = models.ManyToManyField('Appointment', blank=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
+
 
 class Doctor(models.Model):
     # Personal Information
@@ -40,20 +52,20 @@ class Doctor(models.Model):
     email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}: {self.medical_specialization}'
+        return f"{self.first_name} {self.last_name}: {self.medical_specialization}"
 
 
-    
 class Room(models.Model):
     # General Room information
     capacity = models.PositiveIntegerField()
-    temperature = models.IntegerField() 
+    temperature = models.IntegerField()
 
     # Occupant Information
-    priority = models.CharField(max_length=10, choices=[('H', 'High'), ('M', 'Medium'), ('L', 'Low')])
+    priority = models.CharField(
+        max_length=10, choices=[("H", "High"), ("M", "Medium"), ("L", "Low")]
+    )
     is_empty = models.BooleanField(default=False)
-    is_patient_room = models.BooleanField(default= True)
+    is_patient_room = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'Room, Capacity: {self.capacity}'
-
+        return f"Room, Capacity: {self.capacity}"
