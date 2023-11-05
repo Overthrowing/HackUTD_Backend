7827596f582a741e75184dd94c073a1318466259
Siddharth Radhakrishnan <siddharth.radh@gmail.com>
2023-11-04 22:05:11 -0500

    flake8

--- ERBackend/api/serializers.py
+++ ERBackend/api/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = "__all__"
 
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = "__all__"
