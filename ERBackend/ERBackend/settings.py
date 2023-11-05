7168ef42deb3d3196ea156119f6ede28aeac6536
Connor Carey <connorcarey2005@gmail.com>
2023-11-05 05:57:05 -0600

    Added allowed headers

--- ERBackend/ERBackend/settings.py
+++ ERBackend/ERBackend/settings.py
@@ -134,3 +134,25 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "cache-control",
+    "pragma",
+]
