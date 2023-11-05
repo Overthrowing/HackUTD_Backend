8bf225ee86f0e587f009d8da91bc9802c8746833
Siddharth Radhakrishnan <siddharth.radh@gmail.com>
2023-11-04 20:59:23 -0500

    Fixed typo

--- ERBackend/api/urls.py
+++ ERBackend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'api'
 
-# URL patterns for the CyRanch Connect app
+# URL patterns for the ER app
 urlpatterns = [
     path('patients/', views.Patients.as_view(), name='patients'),
 ]
